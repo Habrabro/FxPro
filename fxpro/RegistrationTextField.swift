321a06ae3314f724a7b2c191681fe986c404becd
Антон Карпушко <habrabro@gmail.com>
2019-12-05 00:54:02 +0400

    Added "Residential Address" field section to form

---
+++ fxpro/RegistrationTextField.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationTextField.swift
+//  fxpro
+//
+//  Created by admin on 04/12/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+import TweeTextField
+
+@IBDesignable
+class RegistrationTextField: TweeBorderedTextField {
+
+    
+}
---
+++ fxpro/ViewControllers/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  fxpro
+//
+//  Created by admin on 04/12/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+import TweeTextField
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    @IBOutlet weak var residentialAddressLabel: UILabel!
+    @IBOutlet weak var streetAndNumberTextField: TweeActiveTextField!
+    @IBOutlet weak var postalZipCodeTextField: TweeBorderedTextField!
+    
+    // MARK: Public properties
+    
+    // MARK: Private properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: Public methods
+    
+    // MARK: Actions
+    
+    // MARK: Private methods
+
+}
+
