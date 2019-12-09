858999ff32fb278fb0f08e5b132b4db519dc4ded
Антон Карпушко <habrabro@gmail.com>
2019-12-10 01:24:54 +0400

    Removed comma from "Street And Number" field regex validation pattern

--- fxpro/Validation/ValidatorFactory.swift
+++ fxpro/Validation/ValidatorFactory.swift
@@ -27,7 +27,7 @@ class ValidatorFactory {
     
     static func validatorFor(type: ValidatorType) -> Validator {
         switch type {
-        case .streetAndNumber: return RegexValidator(expression: "^\\s*.+\\s*,\\s*\\d+\\s*$", successMessage: DEFAULT_SUCCESS_MESSAGE, failureMessage: "Provide valid street and number")
+        case .streetAndNumber: return RegexValidator(expression: "^\\s*.+\\s+\\d+\\s*$", successMessage: DEFAULT_SUCCESS_MESSAGE, failureMessage: "Provide valid street and number")
         case .postalZipCode: return RegexValidator(expression: ".+", successMessage: DEFAULT_SUCCESS_MESSAGE, failureMessage: "Provide a valid postal / zip code")
         case .cityTown: return RegexValidator(expression: "^\\s*\\p{L}+(?:[\\s-]\\p{L}+)*\\s*$", successMessage: DEFAULT_SUCCESS_MESSAGE, failureMessage: "Provide a valid city or town name")
         case .nationality: return RegexValidator(expression: ".+", successMessage: DEFAULT_SUCCESS_MESSAGE, failureMessage: "Select your nationality from the list")
